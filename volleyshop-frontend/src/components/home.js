dab45111ef558ae121f87c92fbc641ade6291fdb
psnc.weiss <tweiss@man.poznan.pl>
2020-01-25 12:35:32 +0100

    commit

--- volleyshop-frontend/src/components/home.js
+++ volleyshop-frontend/src/components/home.js
@@ -5,8 +5,10 @@ class Home extends Component {
         return(
             <div className="container">
                 <div className="divide-container">Start</div>
-                <div className="page-under-construction">
-                    
+                <div className="page-under-construction" style={{backgroundImage: 'url(https://s.redefine.pl/file/o2/redefine/cp/mn/mnvtk3v2nmg4nsbr7oduraphzze83m8i.jpg)'}}>
+                    <div style={{width: '100%', height: '100%', backgroundColor: "rgba(0, 0, 0, 0.7)", fontSize: '60px', textAlign: 'center', color: "#ccc", lineHeight: '11.0'}}>
+                        Witaj w VolleyShop
+                    </div>
                 </div>
             </div>
         )
