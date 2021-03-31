26ed823022ddb910a7d2c8c7c9db91412b590d92
Tomasz Weiss <tweiss@man.poznan.pl>
2021-03-31 14:33:24 +0200

    triggers finish

--- volleyshop-backend/src/main/resources/db/migration/V2__Triggers.sql
+++ volleyshop-backend/src/main/resources/db/migration/V2__Triggers.sql
@@ -1,79 +1,164 @@
 -- Uzytkownicy
-CREATE OR REPLACE FUNCTION users_log()
+CREATE OR REPLACE FUNCTION users_log_insert()
 returns trigger as $$
     BEGIN
-        INSERT INTO users_logs VALUES(new.firstName, new.lastName, new.email, new.password, now());
+        INSERT INTO users_logs VALUES('', '', '', '', new.firstName, new.lastName, new.email, new.password, now());
         return NEW;
     end;
 $$
 LANGUAGE plpgsql;
 
-create trigger users_changer
+create trigger users_inserter
     before insert
     on users
     for each row
-    execute procedure users_log();
+    execute procedure users_log_insert();
 
--- Obuwie
-CREATE OR REPLACE FUNCTION shoes_log()
+CREATE OR REPLACE FUNCTION users_log_update()
 returns trigger as $$
     BEGIN
-        INSERT INTO shoes_logs VALUES(new.name, new.gender, new.brand, new.description, new.prize, new.image, now());
+        if new.firstName<>old.firstName or new.lastName<>old.lastName or new.email<>old.email or new.password<>old.password THEN
+            INSERT INTO users_logs VALUES(old.firstName, old.lastName, old.email, old.password, new.firstName, new.lastName, new.email, new.password, now());
+        end if;
+    return NEW;
+end;
+$$
+LANGUAGE plpgsql;
+
+create trigger users_updater
+    before update
+    on users
+    for each row
+    execute procedure users_log_update();
+
+-- Obuwie
+CREATE OR REPLACE FUNCTION shoes_log_insert()
+returns trigger as $$
+    BEGIN
+        INSERT INTO shoes_logs VALUES('', '', '', '', 0.0, '', new.name, new.gender, new.brand, new.description, new.prize, new.image, now());
         return NEW;
     end;
 $$
 LANGUAGE plpgsql;
 
-create trigger shoes_changer
+create trigger shoes_inserter
     before insert
-    on shoes_logs
+    on shoes
     for each row
-    execute procedure shoes_log();
+    execute procedure shoes_log_insert();
+
+CREATE OR REPLACE FUNCTION shoes_log_update()
+returns trigger as $$
+    BEGIN
+        if new.name<>old.name or new.gender<>old.gender or new.brand<>old.brand or new.description<>old.description or new.prize<>old.prize or new.image<>old.image THEN
+            INSERT INTO shoes_logs VALUES(old.name, old.gender, old.brand, old.description, old.prize, old.image, new.name, new.gender, new.brand, new.description, new.prize, new.image, now());
+        end if;
+        return NEW;
+    end;
+$$
+LANGUAGE plpgsql;
+
+create trigger shoes_updater
+    before update
+    on shoes
+    for each row
+    execute procedure shoes_log_update();
 
 -- Ubrania
-CREATE OR REPLACE FUNCTION clothes_log()
+CREATE OR REPLACE FUNCTION clothes_log_insert()
 returns trigger as $$
-BEGIN
-INSERT INTO clothes_logs VALUES(new.name, new.gender, new.brand, new.description, new.prize, new.type, new.image, now());
-return NEW;
-end;
+    BEGIN
+        INSERT INTO clothes_logs VALUES('', '', '', '', 0.0, '', '', new.name, new.gender, new.brand, new.description, new.prize, new.type, new.image, now());
+        return NEW;
+    end;
 $$
 LANGUAGE plpgsql;
 
-create trigger clothes_changer
+create trigger clothes_inserter
     before insert
-    on clothes_logs
+    on clothes
     for each row
-    execute procedure clothes_log();
+    execute procedure clothes_log_insert();
+
+CREATE OR REPLACE FUNCTION clothes_log_update()
+returns trigger as $$
+    BEGIN
+        if new.name<>old.name or new.gender<>old.gender or new.brand<>old.brand or new.description<>old.description or new.prize<>old.prize or new.type<>old.type or new.image<>old.image THEN
+            INSERT INTO clothes_logs VALUES(old.name, old.gender, old.brand, old.description, old.prize, old.type, old.image, new.name, new.gender, new.brand, new.description, new.prize, new.type, new.image, now());
+        end if;
+        return NEW;
+    end;
+$$
+LANGUAGE plpgsql;
+
+create trigger clothes_updater
+    before update
+    on clothes
+    for each row
+    execute procedure clothes_log_update();
 
 -- Akcesoria
-CREATE OR REPLACE FUNCTION accessories_log()
+CREATE OR REPLACE FUNCTION accessories_log_insert()
 returns trigger as $$
 BEGIN
-INSERT INTO accessories_logs VALUES(new.name, new.gender, new.brand, new.description, new.prize, new.type, new.image, now());
+INSERT INTO accessories_logs VALUES('', '', '', '', 0.0, '', '', new.name, new.gender, new.brand, new.description, new.prize, new.type, new.image, now());
 return NEW;
 end;
 $$
 LANGUAGE plpgsql;
 
-create trigger accessories_changer
+create trigger accessories_inserter
     before insert
-    on accessories_logs
+    on accessories
     for each row
-    execute procedure accessories_log();
+    execute procedure accessories_log_insert();
+
+CREATE OR REPLACE FUNCTION accessories_log_update()
+returns trigger as $$
+    BEGIN
+        if new.name<>old.name or new.gender<>old.gender or new.brand<>old.brand or new.description<>old.description or new.prize<>old.prize or new.type<>old.type or new.image<>old.image THEN
+            INSERT INTO accessories_logs VALUES(old.name, old.gender, old.brand, old.description, old.prize, old.type, old.image, new.name, new.gender, new.brand, new.description, new.prize, new.type, new.image, now());
+        end if;
+        return NEW;
+    end;
+$$
+LANGUAGE plpgsql;
+
+create trigger accessories_updater
+    before update
+    on accessories
+    for each row
+    execute procedure accessories_log_update();
 
 -- Suplementy
-CREATE OR REPLACE FUNCTION suplements_log()
+CREATE OR REPLACE FUNCTION supplements_log_insert()
 returns trigger as $$
 BEGIN
-INSERT INTO suplements_logs VALUES(new.name, new.brand, new.description, new.prize, new.type, new.image, now());
+INSERT INTO supplements_logs VALUES('', '', '', 0.0, '', '', new.name, new.brand, new.description, new.prize, new.type, new.image, now());
 return NEW;
 end;
 $$
 LANGUAGE plpgsql;
 
-create trigger suplements_changer
+create trigger supplements_inserter
     before insert
-    on suplements_logs
+    on supplements
     for each row
-    execute procedure suplements_log();
+    execute procedure supplements_log_insert();
+
+CREATE OR REPLACE FUNCTION supplements_log_update()
+returns trigger as $$
+    BEGIN
+        if new.name<>old.name or new.description<>old.description or new.prize<>old.prize or new.type<>old.type or new.image<>old.image THEN
+            INSERT INTO supplements_logs VALUES(old.name, old.brand, old.description, old.prize, old.type, old.image, new.name, new.brand, new.description, new.prize, new.type, new.image, now());
+        end if;
+        return NEW;
+    end;
+$$
+LANGUAGE plpgsql;
+
+create trigger supplements_updater
+    before update
+    on supplements
+    for each row
+    execute procedure supplements_log_update();
